a0efa74b512b60c826994a6478f5ff7386b2517c
Zain Ali <zain3002ali@gmail.com>
2023-02-26 19:03:24 +0000

    Three of a kind created

--- poker.js
+++ poker.js
@@ -19,18 +19,19 @@ for(let i=0; i<5; i++){
     return hand
 }
 
-let NUM_TRIALS = 2 //This is the number of hands to be generated and checked
+let NUM_TRIALS = 5 //This is the number of hands to be generated and checked
 let NUM_flush = 0 //number of times a flush appears.
 let NUM_fullhouse = 0 //number of times a full house appears
-
+let NUM_ThreeOfAkind = 0 //number of times three of a kind appears
 let hand = []
 
 
 // hand = drawFiveCards()
 // console.log(hand.join(' '));
 
-function full_house(){
+function full_house(){ //2 ranks are the same and the other three are aswell.
     let array = [] 
+    let count = 0
     for (let i = 0; i < hand.length; i++){
         if (hand[i][1] == 0){
             array.push("10") 
@@ -38,18 +39,23 @@ function full_house(){
             array.push(hand[i][0]) //Creates new array with just numbers 
         }
     }
-    array.sort((a, b) => a - b) //sorts so that duplicates are next to each other in accending order
-    console.log(array)
-    if ((array[0] == array[1]) && (array[2] == array[3]) && (array[3] == array[4])){ //checks if the first two are the same along with the next three
-        console.log("this is full house")
-        NUM_fullhouse++
-        return NUM_fullhouse 
+    for (let j = 0; j <array.length; j++){
+        for (let k = j + 1; k < array.length; k++){
+            if (array[j] == array[k]){
+                count++
+                if (count == 4){
+                    console.log("this is a fullhouse")
+                    NUM_fullhouse++
+                    return NUM_fullhouse 
+                }
+            }
+        }
     }
 }
 
 
 
-function flush(){
+function flush(){ //five cards of the same suit
     let count = 0
     for (let i = 0; i < hand.length; i++){ //finding duplicates by comparing each element against each other
         for (let j = i + 1 ; j < hand.length; j++) {
@@ -68,7 +74,30 @@ function flush(){
     }
 }
 
-
+function three_of_a_kind(){ //three cards of the same rank and two other random ones
+    let array = [] 
+    let count = 0
+    for (let i = 0; i < hand.length; i++){
+        if (hand[i][1] == 0){
+            array.push("10") 
+        }else{
+            array.push(hand[i][0]) //Creates new array with just numbers 
+        }
+    }
+    for (let j = 0; j <array.length; j++){
+        for (let k = j + 1; k < array.length; k++){ //again checking each value against each other
+            if (array[j] == array[k]){ //counting the number of repeats 
+                count++
+                if (count == 3){
+                    console.log("this is three of a kind")
+                    NUM_ThreeOfAkind++
+                    return NUM_ThreeOfAkind 
+                }
+            }
+        }
+    }
+}
+//this function is similar to the full house so i just used the same code and changed the logic towards the end.
 
 
 
@@ -77,11 +106,5 @@ for(let i=0; i<NUM_TRIALS; i++){ // loop to check each hand againt the special c
     console.log(hand)
     flush()
     full_house()
+    three_of_a_kind()
 }
-
-
-//set suits value in nested array equal to 5 different variables and compare them
-// or
-//seperate nested array tyop just suits and numbers
-
-//ADD SUITS TO A NEW ARRAY WITHIN THE FUNCTION
